c59b8f0f0696ab46de7d4623e64f130a15ce9db0
BingoJYB <7303639+BingoJYB@users.noreply.github.com>
2018-09-14 14:26:14 +0200

    Add files via upload

--- Support Vector Machine/svm.py
+++ Support Vector Machine/svm.py
@@ -35,17 +35,65 @@ def Q16_Q17(x, y):
 def Q18(x_train, y_train, x_test, y_test):
     sv_num = []
     Eout_list = []
-    
+
     for c in [0.001, 0.01, 0.1, 1, 10]:
         clf = svm.SVC(C=c, kernel='rbf', gamma=100)
         clf.fit(x_train, y_train)
-        
+
         predict_y = clf.predict(x_test)
-        
+
         sv_num.append(len(clf.support_vectors_))
         Eout_list.append(calclute_error(predict_y, y_test))
-        
+
     return sv_num, Eout_list
+
+
+def Q19(x_train, y_train, x_test, y_test):
+    Eout_min = math.inf
+
+    for ga in [1, 10, 100, 1000, 10000]:
+        clf = svm.SVC(C=0.1, kernel='rbf', gamma=ga)
+        clf.fit(x_train, y_train)
+
+        predict_y = clf.predict(x_test)
+        Eout = calclute_error(predict_y, y_test)
+
+        if Eout < Eout_min:
+            Eout_min = Eout
+            gamma = ga
+
+    return gamma
+
+
+def Q20(dataframe):
+    gamma_list = []
+
+    for i in range(100):
+        Eval_min = math.inf
+        df = dataframe.sample(frac=1)
+        df_before_1000 = df[:1000]
+        df_after_1000 = df[1000:]
+
+        x_train = list(zip(df_before_1000['intensity'], df_before_1000['symmetry']))
+        y_train = df_before_1000['tag']
+
+        x_test = list(zip(df_after_1000['intensity'], df_after_1000['symmetry']))
+        y_test = df_after_1000['tag']
+
+        for ga in [1, 10, 100, 1000, 10000]:
+            clf = svm.SVC(C=0.1, kernel='rbf', gamma=ga)
+            clf.fit(x_train, y_train)
+
+            predict_y = clf.predict(x_test)
+            Eval = calclute_error(predict_y, y_test)
+
+            if Eval < Eval_min:
+                Eval_min = Eval
+                gamma = ga
+
+        gamma_list.append(gamma)
+
+    return pd.DataFrame(gamma_list, columns=['gamma'])
 
 
 # Q15
@@ -88,7 +136,12 @@ sv_num, Eout_list = Q18(x_train, y_train, x_test, y_test)
 print(sv_num)
 print(Eout_list)
 
+# Q19
+gamma = Q19(x_train, y_train, x_test, y_test)
 
+print(gamma)
 
-
-
+# Q20
+dataframe = divide_digit(read_file('train.txt'), 0)
+gamma_list = Q20(dataframe)
+print(gamma_list.groupby('gamma')['gamma'].count())
